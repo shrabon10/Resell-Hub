ed29cd1646c29bf7a344917d11bf5d1ef9514f2a
shrabon10 <omarfaruksrabon@gmail.com>
2026-06-28 12:06:16 +0600

    Error Handeling Successfully Coverd

--- src/app/layout.js
+++ src/app/layout.js
@@ -26,7 +26,7 @@ export const viewport = {
 export default function RootLayout({ children }) {
   return (
     <html
-      lang="en"
+      lang="en" suppressHydrationWarning
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
